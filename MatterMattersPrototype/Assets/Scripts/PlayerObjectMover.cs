2c380e7bfa2814e2786bf5985c7b0c9dbaeb5e2a
Flopss <Flo.Baier@gmx.de>
2020-03-10 17:03:24 +0100

    Adds Player movement. Adds Camera follows Player

---
+++ MatterMattersPrototype/Assets/Scripts/PlayerObjectMover.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObjectMover : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _base = null;
+
+    [SerializeField]
+    private float _speed = 1f;
+
+    [SerializeField]
+    private Rigidbody2D _rigidbody = null;
+
+    private const float _speedFactor = 0.001f;
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        if(Input.GetMouseButton(0))
+        {
+            setVelocity();
+        }
+    }
+
+    private Vector2 calculateMouseDistance()
+    {
+        Vector2 position = Camera.main.WorldToScreenPoint(_base.position);
+        Vector2 mousePosition = Input.mousePosition;
+
+        return mousePosition - position;
+    }
+
+    private void setVelocity()
+    {
+        Vector2 mouseDistance = calculateMouseDistance();
+        _rigidbody.velocity = calculateSpeed(mouseDistance) * mouseDistance.normalized;
+        Debug.Log(_rigidbody.velocity);
+    }
+
+    private float calculateSpeed(Vector2 distance)
+    {
+        return distance.magnitude * _speed * _speedFactor;
+    }
+}
---
+++ MatterMattersPrototype/Assets/Scripts/PlayerPositionFollower.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPositionFollower : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _objectToFollow = null;
+
+    [SerializeField]
+    private Transform _base = null;
+
+    protected virtual void Update()
+    {
+        Vector3 pos = _objectToFollow.position;
+        _base.position = new Vector3(pos.x, pos.y, _base.position.z);
+    }
+}
